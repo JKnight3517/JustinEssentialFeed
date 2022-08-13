c33e977a0537b4e43970792815079cf99275289b
JKnight3517 <knightjknight@gmail.com>
2022-08-13 12:24:47 -0700

    Initial Commit

---
+++ JustinEssentialFeed/JustinEssentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  JustinEssentialFeed.h
+//  JustinEssentialFeed
+//
+//  Created by Justin Knight on 8/13/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JustinEssentialFeed.
+FOUNDATION_EXPORT double JustinEssentialFeedVersionNumber;
+
+//! Project version string for JustinEssentialFeed.
+FOUNDATION_EXPORT const unsigned char JustinEssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustinEssentialFeed/PublicHeader.h>
+
+
